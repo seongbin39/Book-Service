7c041650c2bcc0b2efb08deee04a659896639e4e
seongbin3931 <seongbin3931@gmail.com>
2021-10-21 16:04:18 +0900

    Book-service ver1.1

--- server/src/models/Book.js
+++ server/src/models/Book.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
 const bookSchema = mongoose.Schema({
-    title: { type: 'string', required: true},
-    author: { type: 'string', required: true},
-    summary: { type: 'string'},
+    title: { type: 'string', required: true, trim: true},
+    author: { type: 'string', required: true, trim: true},
+    summary: { type: 'string', trim: true},
     genre: { type: 'string'},
-    release: { type: 'string', required: true},
-    ISBN: { type: 'Number', required: true}
+    release: { type: 'string', required: true, trim: true},
+    ISBN: { type: 'Number', required: true, trim: true}
 })
 
 const Book = mongoose.model('Book', bookSchema)
